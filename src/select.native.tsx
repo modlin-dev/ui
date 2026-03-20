e81d97db83bb17ebddfac1028ac98f84fe7e3c0b
Farhan Kabir <farhan@modlin.dev>
2026-03-20 23:01:59 +0600

    feat: contains self globals.css file

--- src/select.native.tsx
+++ src/select.native.tsx
@@ -3,14 +3,14 @@ import { cn } from "./utils"
 import type { ViewProps } from "./globals"
 
 const size = {
-    sm: "h-8 px-2.5 text-sm rounded-lg",
-    md: "h-9 px-3 rounded-[9px] text-sm",
-    lg: "h-11 px-3 rounded-[14px]",
-    xl: "h-12 px-4 rounded-2xl",
+	sm: "h-8 px-2.5 text-sm rounded-lg",
+	md: "h-9 px-3 rounded-[9px] text-sm",
+	lg: "h-11 px-3 rounded-[14px]",
+	xl: "h-12 px-4 rounded-2xl"
 }
 
 export interface SelectProps extends ViewProps {
-    size?: keyof typeof size
+	size?: keyof typeof size
 	defaultValue?: string
 	name?: string
 	id?: string
@@ -27,8 +27,8 @@ export function Select(props: SelectProps) {
 				"flex items-center bg-background/25 backdrop-blur-sm inset-ring inset-ring-border",
 				"open:inset-ring-primary/75 open:ring-4 open:ring-primary/10 dark:open:ring-primary/15 focus:inset-ring-primary/75 focus:ring-4 focus:ring-primary/10 dark:focus:ring-primary/15",
 				"transition duration-150 ease-in",
-                size[props.size ?? "xl"],
-                props.className
+				size[props.size ?? "xl"],
+				props.className
 			)}
 		>
 			{props.children}
@@ -47,10 +47,11 @@ export interface SelectItemProps {
 export function SelectItem(props: SelectItemProps) {
 	return (
 		<option
+			dir="rtl"
 			className={cn(
-				"gap-4 h-9 min-h-9 px-3 rounded-lg text-foreground hover:bg-secondary focus:bg-secondary checked:bg-secondary",
+				"justify-between gap-4 h-9 min-h-9 px-3 rounded-lg text-foreground hover:bg-secondary focus:bg-secondary checked:bg-secondary",
 				"transition duration-150 ease-out",
-				props.className,
+				props.className
 			)}
 			value={props.value}
 			disabled={props.disabled}

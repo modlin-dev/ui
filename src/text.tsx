d0b8983a1dd6e62578b1ca2e5e351c4cb97a72b1
Farhan Kabir <farhan@modlin.dev>
2026-03-21 10:06:34 +0600

    fix: text component now uses inherit

--- src/text.tsx
+++ src/text.tsx
@@ -1,9 +1,7 @@
-import { cn } from "./utils"
-
 export interface TextProps {
 	className?: string
 	children?: React.ReactNode
 }
 export default function Text(props: Readonly<TextProps>) {
-	return <p className={cn("text-[13px]/4.5", props.className)}>{props.children}</p>
+	return <p className={props.className}>{props.children}</p>
 }
